bdb01aea801c30c69378f7f451cfc920c0744444
Sugatraj <sugatraj.2106@gmail.com>
2025-08-13 11:55:04 +0530

    checked some error handlings

--- app/main.py
+++ app/main.py
@@ -4,20 +4,46 @@ import random
 
 app = FastAPI()
 
-@app.get("/readme")
-def readme() -> dict[str, Any]:
-    random_name = ["Coolie Powerhouse", "Say My Name", "God Damn Right"]
-    random_badge_no = ["1421", "786", "131313", "007", "1950"]
-    return {
-        "name" : random.choice(random_name),
-        "badge_no" : random.choice(random_badge_no)
+shipments = {
+    121: {
+    "name" : "Coolie Powerhouse",
+    "badge_no" : "1421",
+    "shipment_status" : "Powerful"
+    },
+    122: {
+    "name" : "Say My Name",
+    "badge_no" : "786",
+    "shipment_status" : "Delivered"
+    },
+    123: {
+    "name" : "God Damn Right",
+    "badge_no" : "131313",
+    "shipment_status" : "In Transit"
+    },
+    124: {
+    "name" : "Breaking Bad",
+    "badge_no" : "007",
+    "shipment_status" : "Pending"
+    },
+    125: {
+    "name" : "Walter White",
+    "badge_no" : "1950",
+    "shipment_status" : "Cancelled"
+    },
+    126: {
+    "name" : "Jesse Pinkman",
+    "badge_no" : "999",
+    "shipment_status" : "Returned"
     }
+}
+
+@app.get("/latest")
+def latest() -> dict[str, Any]:
+    id = max(shipments.keys())
+    return shipments[id]
 
 @app.get("/shipment/{id}")
 def shipmentData(id : int) -> dict[str, Any | list[str]]:
-    random_status = ["ok", "not so ok", "ok ok"]
-    random_id = random.randint(1,121)
-    return {
-        "shipment_id" : random_id,
-        "shipment_status" : random.choice(random_status)
-    }
+    if id not in shipments:
+        return {"error" : f"Shipment for id : {id} not found"}
+    return shipments[id]
