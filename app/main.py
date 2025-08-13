970fc6f4dc328f2ab101c14797857cb7ea06ed78
Sugatraj <sugatraj.2106@gmail.com>
2025-08-13 11:37:59 +0530

    sequence matter to work apis static first dynamic later

--- app/main.py
+++ app/main.py
@@ -4,6 +4,15 @@ import random
 
 app = FastAPI()
 
+@app.get("/readme")
+def readme() -> dict[str, Any]:
+    random_name = ["Coolie Powerhouse", "Say My Name", "God Damn Right"]
+    random_badge_no = ["1421", "786", "131313", "007", "1950"]
+    return {
+        "name" : random.choice(random_name),
+        "badge_no" : random.choice(random_badge_no)
+    }
+
 @app.get("/shipment/{id}")
 def shipmentData(id : int) -> dict[str, Any | list[str]]:
     random_status = ["ok", "not so ok", "ok ok"]
